ae958beee2ff6fa024dfa2ac1c6bd5c5a0da12e2
vinny <reid.paris@maine.edu>
2018-07-10 10:33:56 -0400

    Timing function for different parts of code, courtesy of George

---
+++ R/timing_function.R
@@ -0,0 +1,27 @@
+timed = function(ltime = proc.time()[3]) {
+  ## use: t_name = t = timed(t)
+  ##     this saves t_name and keeps t for next timed(t)
+  ## returns two components:
+  ##     seconds from start of process
+  ##     seconds difference from first element of ltime
+  time = proc.time()[3]
+  c(time, time - ltime[1])
+}
+
+
+t_code1 = t_code2 = NULL
+t0 = timed()
+
+x <- rnorm(5e7)
+t_code1 = (t1 = timed(t0))[2]
+t_code1
+
+
+t_total = timed(t0)[2]
+
+comm.cat(" Section  time\n",
+         "  code1", t_code1, "\n",
+         "  code2", t_code2, "\n",
+         "  code3", t_code3, "\n",
+         "  total", t_total, "\n",
+         "\n", all.rank=TRUE)
