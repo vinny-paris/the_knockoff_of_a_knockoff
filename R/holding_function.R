b6218efbdf13cb667b0a488a52e5b40e5f6f98f3
vinny <reid.paris@maine.edu>
2018-07-16 08:47:45 -0400

    Updated the parameter annotations

--- R/holding_function.R
+++ R/holding_function.R
@@ -9,9 +9,12 @@
 #' does not allow for multi-cores. 
 #' 
 #' @export
-#' @param expl The data frame to be the explanatory variables, the first col must be the identifier 
 #' @param resp The response variable, a matrix the same length as expl The first col must be the identifier
+#' @param expl The data frame to be the explanatory variables, the first col must be the identifier 
 #' @param Xko The knockoff variables created seperately (see the 'knockoff' package for more details). Also must be the same length as expl, resp.
+#' @param offset The offset is used to control conservative-ness. 0 for liberal and best for smaller data sets, 1 for conservative and larger data sets. 1 can be a very poor choice if you do not have many, many explanatory variables. Passed to the my_kn function.
+#' @param fdr The "False Discovery Rate", allowed to be between 0,1. Defaults to .2, implying that 1/5 of variables returned will be a type 1 error. This will be passed to the my_kn function.
+#' @param cores The cores to be used in the doMC section of the code, passed to the my_kn function.
 #' @param ... Various other arguments to be passed to the my_kn function.
 #' 
 #' @return This will return of data frame 5 columns. The first being phenotype, the signficant variable selected, the estimate of a glm regression, se for standard error of the regression and the p-value. 
