c6527a605da83dc2ce709f52c135505c37ece5e4
vinny <reid.paris@maine.edu>
2018-07-16 10:51:47 -0400

    Added documentation for Clincial Traits data file (phenome type file)

---
+++ R/ClinicalTraits.R
@@ -0,0 +1,13 @@
+#' File Containing the Phenome Data
+#'
+#' This file contains information on the mice including phenomtypes and identifiers.
+#' @format A data frame with 361 rows by 38 cols. There is some needed cleaning though which the working_mod.R example script runs through.
+#' \describe{
+#'   \item{Mice}{unique identifier}
+#'   ...
+#' }
+#' @source \usr{http://www.genetics.ucla.edu/labs/horvath/CoexpressionNetwork/Rpackages/WGCNA/}
+
+"ClinicalTraits.csv"
+
+
