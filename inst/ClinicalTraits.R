4d9fb466eb50d9f74581f69839c98faf87565084
vinny <reid.paris@maine.edu>
2018-07-17 09:20:26 -0400

    Moved Clinical Triats man file to insist

--- inst/ClinicalTraits.R
+++ inst/ClinicalTraits.R
@@ -1,11 +1,15 @@
 #' File Containing the Phenome Data
 #'
 #' This file contains information on the mice including phenomtypes and identifiers.
+#' 
+#' @export
+#' 
 #' @format A data frame with 361 rows by 38 cols. There is some needed cleaning though which the working_mod.R example script runs through.
 #' \describe{
 #'   \item{Mice}{unique identifier}
 #'   ...
 #' }
+#' 
 #' @source \usr{http://www.genetics.ucla.edu/labs/horvath/CoexpressionNetwork/Rpackages/WGCNA/}
 
 "ClinicalTraits.csv"
